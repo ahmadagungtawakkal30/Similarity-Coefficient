249d7e3fb33625e005967fe76a1d733a4a61a7bd
Ahmad Agung Tawakkal <51843028+ahmadagungtawakkal30@users.noreply.github.com>
2022-09-20 08:39:50 +0700

    ad

--- SC.py
+++ SC.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
 
-a = 1
-b = 2
-c = a+b
-x = ["10","2","3"]
+df = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
 
-st.markdown('asdasdasd')
+c = alt.Chart(df).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.write(c)
