d2d12901ca9d1c9a2a8a146331c6e75a7d8f28ea
Ahmad Agung Tawakkal <51843028+ahmadagungtawakkal30@users.noreply.github.com>
2022-09-20 19:11:45 +0700

    file

--- SC.py
+++ SC.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-df = pd.DataFrame(
-   np.random.randn(50, 20),
-   columns=('col %d' % i for i in range(20)))
+df = pd.read_csv (r'\comet.csv')
+print (df)
 
-st.dataframe(df)  # Same as st.write(df)
+# df = pd.DataFrame(
+#    np.random.randn(50, 20),
+#    columns=('col %d' % i for i in range(20)))
+
+# st.dataframe(df)  # Same as st.write(df)
