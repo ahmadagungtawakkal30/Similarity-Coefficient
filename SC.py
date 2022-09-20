c4a31c6d6b28d22fc4580ad77d874025475c5351
Ahmad Agung Tawakkal <51843028+ahmadagungtawakkal30@users.noreply.github.com>
2022-09-20 19:19:20 +0700

    a

--- SC.py
+++ SC.py
@@ -5,7 +5,7 @@ df = pd.read_csv (r'comet.csv')
 
 mantap = pd.DataFrame (df)
 
-st.dataframe(mantap)
+st.dataframe(df, 50,2)
 # df = pd.DataFrame(
 #    np.random.randn(50, 20),
 #    columns=('col %d' % i for i in range(20)))
