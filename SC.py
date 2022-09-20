0eefe7e23902194c40dcd7606c7ec2c9fd369f9d
Ahmad Agung Tawakkal <51843028+ahmadagungtawakkal30@users.noreply.github.com>
2022-09-20 19:16:40 +0700

    ad

--- SC.py
+++ SC.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv (r'comet.csv')
-st.dataframe (df)
 
+mantap = pd.DataFrame (df,columns=('col %d' % i for i in range(20)))
+
+st.dataframe(mantap)
 # df = pd.DataFrame(
 #    np.random.randn(50, 20),
 #    columns=('col %d' % i for i in range(20)))
